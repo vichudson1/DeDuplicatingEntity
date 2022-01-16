200235dd4f14d7471e6540c782407cbe05badd13
Vic Hudson <vichudson1@gmail.com>
2022-01-16 13:56:18 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# DeDuplicatingEntity
+
+A description of this package.
---
+++ Sources/DeDuplicatingEntity/DeDuplicatingEntity.swift
@@ -0,0 +1,6 @@
+public struct DeDuplicatingEntity {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
